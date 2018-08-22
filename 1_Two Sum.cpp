d20ff76e9e98ca23a72792862ed49f5c2362177d
zhanghan <455209990@qq.com>
2018-08-22 16:32:34 +0800

    two sum

---
+++ 1_Two Sum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+{
+       unordered_map<int, int>m;
+        vector<int> res;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            m[nums[i]] = i;
+        }
+        for(int i = 0; i <nums.size(); i++)
+        {
+            int t = target - nums[i];
+            if(m.count(t) && m[t] != i)
+            {
+                res.push_back(i);
+                res.push_back(m[t]);
+                break;
+            }
+        }
+        return res;
+}
+};
+
+
+int main(int argc, char *argv[])
+{
+    vector<int> a  = {2,5,7,6},b;
+    int target = 9;
+    Solution solution;
+    b = solution.twoSum(a,target);
+    cout<<b[0]<<'\t'<<b[1]<<endl;
+    return 0;
+}
