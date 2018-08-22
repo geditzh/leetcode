747b8106050315ee71ba2c865740eda54df08237
zhanghan <455209990@qq.com>
2018-08-22 17:14:16 +0800

    一遍哈希表

--- 1_Two Sum.cpp
+++ 1_Two Sum.cpp
@@ -10,21 +10,18 @@ public:
 {
        unordered_map<int, int>m;
         vector<int> res;
-        for(int i = 0; i<nums.size(); i++)
-        {
-            m[nums[i]] = i;
-        }
         for(int i = 0; i <nums.size(); i++)
         {
+            m[nums[i]] = i;
             int t = target - nums[i];
             if(m.count(t) && m[t] != i)
             {
-                res.push_back(i);
                 res.push_back(m[t]);
-                break;
+                res.push_back(i);
+                return res;
             }
         }
-        return res;
+
 }
 };
 
